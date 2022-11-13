c6eb48852b4fa3acac1de7dea45093c8b3a9d0e9
VietAnh188 <anhkun123456@gmail.com>
2022-11-13 10:00:58 +0700

    add room number into room

---
+++ BackEnd/seeders/20221113025857-add-roomnumber-into-room.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.addColumn('Policies', 'roomNumber', {
+        type: Sequelize.DataTypes.STRING
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.removeColumn('Policies', 'roomNumber')
+  }
+};
