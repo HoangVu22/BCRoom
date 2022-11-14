90506ae3e2dd1d62e26f4e053a78dd5522bbf706
VietAnh188 <anhkun123456@gmail.com>
2022-11-14 21:59:19 +0700

    install cors

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const http = require('http')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const v1 = require('./api/v1')
 
@@ -11,6 +12,7 @@ const port = process.env.SERVER_PORT
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(cors())
 
 // connected app with api
 v1(app)
