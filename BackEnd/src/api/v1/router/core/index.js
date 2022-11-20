8e7595fac87ddd6b807982c5edd1b053633a514e
VietAnh188 <anhkun123456@gmail.com>
2022-11-20 19:46:03 +0700

    check login middleware

--- BackEnd/src/api/v1/router/core/index.js
+++ BackEnd/src/api/v1/router/core/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router()
 const controllers = require('../../controller')
+const { isLogin }= require('../../../../middleware')
 
-router.post('/new_room', controllers.createNewRoomInHotel)
-router.post('/booking', controllers.bookingRoom)
+router.post('/new_room', isLogin, controllers.createNewRoomInHotel)
+router.post('/booking', isLogin, controllers.bookingRoom)
 
 module.exports = {
     entry: '/core',
