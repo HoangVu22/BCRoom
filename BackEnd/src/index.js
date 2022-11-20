92059b11781c1946b4ed99b5c61d7ef6a22d4f07
VietAnh188 <anhkun123456@gmail.com>
2022-11-20 20:56:47 +0700

    app.all

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -16,6 +16,13 @@ app.use(bodyParser.json())
 app.use(cors())
 app.use(cookieParser())
 
+app.all("*",  (req, res, next) =>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+});
+
 // connected app with api
 v1(app)
 
