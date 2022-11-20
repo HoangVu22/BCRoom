d751b2471c81a396c26e815df4d2c79fb554f602
VietAnh188 <anhkun123456@gmail.com>
2022-11-20 22:27:38 +0700

    change entry

--- BackEnd/src/api/v1/router/room/index.js
+++ BackEnd/src/api/v1/router/room/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const controllers = require('../../controller')
 
-router.get('/by_hotel_id', controllers.getRoomByHotelId)
+router.get('/by_hotel_id/:hotelId', controllers.getRoomByHotelId)
 
 module.exports = {
     entry: '/rooms',
