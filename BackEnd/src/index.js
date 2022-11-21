2d9688fb2f23f8c83df3ff64760be493ca6b5251
VietAnh188 <anhkun123456@gmail.com>
2022-11-21 13:20:44 +0700

    fix cors origin

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -13,7 +13,7 @@ const port = process.env.SERVER_PORT || 1234
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-app.use(cors({ credentials: true, origin: 'http://localhost:5500' }))
+app.use(cors({ credentials: true, origin: 'http://localhost' }))
 app.use(cookieParser())
 
 app.all("*",  (req, res, next) =>{
