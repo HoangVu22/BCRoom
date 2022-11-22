71a8decb4d54df4c71e047679425ca7d2936cef8
VietAnh188 <anhkun123456@gmail.com>
2022-11-22 21:42:58 +0700

    update review hotel api

--- BackEnd/src/api/v1/controller/review/reviewHotel.js
+++ BackEnd/src/api/v1/controller/review/reviewHotel.js
@@ -1,27 +1,45 @@
-const { Review } = require('../../../../../models')
+const { Review, Customer, Image } = require('../../../../../models');
 
 module.exports = async (request, response) => {
     try {
-        const { hotelId, starNumber, content } = request.body
+        const { hotelId, starNumber, content } = request.body;
         // const customerId = request.session.userId 
-        const customerId = request.body.customerId
+        const customerId = request.body.customerId;
+
+        const review = await Review.create({
+            hotelId,
+            customerId,
+            starNumber,
+            content
+        });
+
+        const customer = await Customer.findByPk(customerId);
+        const avatar = await Image.findOne({
+            where: {
+                customerId
+            },
+            attributes: ['url']
+        });
+
+        const result = {
+            content: review.content,
+            starNumber: review.starNumber,
+            createdAt: review.createdAt,
+            customerName: customer.username,
+            avatar: avatar?.url 
+        };
 
         return response.status(200).json({
             code: 200,
             status: 'success',
-            data: await Review.create({
-                hotelId,
-                customerId,
-                starNumber,
-                content
-            })
-        })
+            data: result
+        });
     } catch (error) {
-        console.log(error)
+        console.log(error);
         return response.status(500).json({
             code: 500,
             status: 'failed',
             message: error
-        }) 
+        });
     }
-}
+};
