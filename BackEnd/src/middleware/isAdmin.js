9f114cd356afb33e374f1523529fe0453e106e34
VietAnh188 <anhkun123456@gmail.com>
2022-12-01 11:25:59 +0700

    fix isadmin middleware

--- BackEnd/src/middleware/isAdmin.js
+++ BackEnd/src/middleware/isAdmin.js
@@ -1,8 +1,16 @@
-const { Customer, Role } = require('../../models')
+const { Customer, Role } = require('../../models');
 
-async function isAdmin(request, response, next) {
+async function isAdmin (request, response, next) {
     try {
-        const customerId = request.body.customerId
+        const customerId = request.body.customerId;
+
+        if (!customerId) {
+            return response.status(401).json({
+                code: 401,
+                status: 'failed',
+                message: 'you are not logged in'
+            })
+        }
 
         const customer = await Customer.findOne({
             where: {
@@ -11,25 +19,33 @@ async function isAdmin(request, response, next) {
             include: {
                 model: Role
             }
-        })
+        });
+
+        if (!customer) {
+            return response.status(404).json({
+                code: 404,
+                status: 'failed',
+                message: 'where did you get this id, no find customer with it'
+            });
+        }
 
         if (customer.dataValues.Role.dataValues.roleName !== 'admin') {
             return response.status(403).json({
                 code: 403,
                 status: 'failed',
                 message: 'you are not admin'
-            })
+            });
         }
 
-        next()
+        next();
     } catch (error) {
-        console.log(error)
+        console.log(error);
         return response.status(500).json({
             code: 500,
             status: 'failed',
             message: error
-        }) 
+        });
     }
 }
 
-module.exports = isAdmin
+module.exports = isAdmin;
